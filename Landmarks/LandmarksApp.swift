b7dc09a0d7bfdccfbf71c3568015a684959435be
Louis Fernando <ferlouis55@gmail.com>
2024-04-18 11:54:07 +0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by student on 18/04/24.
+//
+
+import SwiftUI
+
+@main
+// @main untuk mengidentifikasikan sebagai poin awal dari aplikasi
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
