755265a089987f7fb503b5e783aed4318b18779d
Louis Fernando <ferlouis55@gmail.com>
2024-04-19 09:24:58 +0700

    Chapter1_BuildingListandNavigation(DONE)

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,38 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        // secara default VStack akan meletakkan isi di dalamnya secara center pada x-axis dan y-axis, tapi bisa diatur alignment nya
-        VStack {
-            MapView()
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                
-                HStack {
-                    Text("Joshua Tree International Park")
-                    Spacer()
-                    Text("California")
-                }
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
-                
-                Divider()
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                
-                Text("Descriptive text goes here.")
-            }
-            .padding()
-            
-            Spacer()
-        }
+       LandmarkList()
     }
 }
 
