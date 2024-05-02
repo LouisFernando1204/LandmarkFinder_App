b18b567e69cca9128d5df8af40f50df763319415
Louis Fernando <ferlouis55@gmail.com>
2024-05-02 12:30:11 +0700

    Chapter4_InterfacingWithUIKit(DONE)

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -11,24 +11,22 @@ struct CategoryHome: View {
     @Environment(ModelData.self) var modelData
     @State private var showingProfile = false
 
-    var body: some View {
-       NavigationSplitView {
-           List {
-               modelData.features[0].image
-                  .resizable()
-                  .scaledToFill()
-                  .frame(height: 200)
-                  .clipped()
-                  .listRowInsets(EdgeInsets())
 
-               ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
-                   CategoryRow(categoryName: key, items: modelData.categories[key]!)
+    var body: some View {
+        NavigationSplitView {
+            List {
+                PageView(pages: modelData.features.map { FeatureCard(landmark: $0) })
+                    .listRowInsets(EdgeInsets())
+
+
+                ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
-               .listRowInsets(EdgeInsets())
-           }
-           .listStyle(.inset)
-           .navigationTitle("Featured")
-           .toolbar {
+                .listRowInsets(EdgeInsets())
+            }
+            .listStyle(.inset)
+            .navigationTitle("Featured")
+            .toolbar {
                 Button {
                     showingProfile.toggle()
                 } label: {
