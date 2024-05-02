f72d458bcf28e473d30221e94f7e3097501b4a29
Louis Fernando <ferlouis55@gmail.com>
2024-05-02 13:21:19 +0700

    Chapter4_CreatingAWatchOSApp(DONE)

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -18,5 +18,8 @@ struct LandmarksApp: App {
             ContentView()
                 .environment(modelData)
         }
+    #if os(watchOS)
+    WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
+    #endif
     }
 }
